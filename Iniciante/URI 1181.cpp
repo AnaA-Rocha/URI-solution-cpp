3ed3b6dc3886bdc9287cabb1ad32da7b403cbc80
Ana Aline Rocha <68602933+AnaA-Rocha@users.noreply.github.com>
2020-10-30 19:17:26 -0300

    Create URI 1181.cpp

---
+++ Iniciante/URI 1181.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	float M[12][12];
+	int l,c,n,soma=0;
+	float media;
+	char s;
+	cin>>n;
+	cin>>s;
+	for(l=0;l<=12;l++)
+	{
+		for(c=0;c<12;c++)
+		{
+			cin>>M[l][c];
+		}
+	}
+	for(l=n;l<=n;l++)
+	{
+		for(c=0;c<12;c++)
+		{
+		    soma+=M[l][c];
+		    media=soma/12.0;
+		}
+	}
+	if(s=='S')
+	{
+		cout.precision(1);
+		cout<<fixed<<(soma*10.0)/10<<endl;
+	}
+	else
+	{
+		cout.precision(1);
+		cout<<fixed<<media<<endl;
+	}
+}
