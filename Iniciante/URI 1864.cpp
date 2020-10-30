d13165067bd121bfdb9e569a60a82636e1e05f95
Ana Aline Rocha <68602933+AnaA-Rocha@users.noreply.github.com>
2020-10-30 19:27:15 -0300

    Create URI 1864.cpp

---
+++ Iniciante/URI 1864.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+	string a="LIFE IS NOT A PROBLEM TO BE SOLVED";
+	int n;
+	scanf(" %d",&n);
+	for(int i=0;i<n;i++)
+		cout<<a[i];
+	cout<<endl;
+}
