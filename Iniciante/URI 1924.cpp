d3cd4b5fe4e902d16ea1a73e8d11bb9a029b35d8
Ana Aline Rocha <68602933+AnaA-Rocha@users.noreply.github.com>
2020-10-30 19:28:03 -0300

    Create URI 1924.cpp

---
+++ Iniciante/URI 1924.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+	int a;
+	scanf(" %d",&a);
+	while(a--)
+	{
+		char b[1000000];
+		scanf(" %[^\n]",b);
+	}
+	printf("Ciencia da Computacao\n");
+}
