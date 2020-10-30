215eb309b8f56d9f382929b02f2cfc8cb7a3a1d6
Ana Aline Rocha <68602933+AnaA-Rocha@users.noreply.github.com>
2020-10-30 19:03:47 -0300

    Create URI 1113.cpp

---
+++ Iniciante/URI 1113.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int x,y;
+	do
+	{
+		cin>>x>>y;
+		if(x>y)
+			cout<<"Decrescente"<<endl;
+		else if(x<y)
+			cout<<"Crescente"<<endl;
+	}
+	while (x!=y);
+}
