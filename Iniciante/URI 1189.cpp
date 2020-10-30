9a2043de8ba4e46951d72a5a86b12ccbe46c6706
Ana Aline Rocha <68602933+AnaA-Rocha@users.noreply.github.com>
2020-10-30 19:21:51 -0300

    Create URI 1189.cpp

---
+++ Iniciante/URI 1189.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	char s;
+	double M[12][12];
+	int c,l;
+	double soma=0;
+	cin>>s;
+	for(l=0;l<12;l++)
+	{
+		for(c=0;c<12;c++)
+		{
+			cin>>M[l][c];
+		}
+	}
+	for(l=0;l<12;l++)
+	{
+		for(c=0;c<12;c++)
+		{
+			if(l+c<11 && c<l)
+			{
+					soma+=M[l][c];
+			}
+		}
+	}
+	if(s=='S')
+	{
+		cout.precision(1);
+		cout<<fixed<<soma<<endl;
+	}
+	else
+	{
+		cout.precision(1);
+		cout<<fixed<<soma/30.0<<endl;
+	}
+}
