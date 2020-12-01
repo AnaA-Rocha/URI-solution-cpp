5a931a81c40068882fa0d2f3fb0020686808990b
Ana Aline Rocha <68602933+AnaA-Rocha@users.noreply.github.com>
2020-12-01 13:23:53 -0300

    Create URI 2235.cpp

---
+++ Iniciante/URI 2235.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a, b, c;
+	cin>>a>>b>>c;
+	if(a-b == 0 || a-c == 0 || b-c == 0 || (a+b)-c == 0 || 
+		 (b+c)-a == 0 || (a+c)-b == 0){
+			cout<<"S"<<endl;
+	}
+	else{
+		cout<<"N"<<endl;
+	}
+}
